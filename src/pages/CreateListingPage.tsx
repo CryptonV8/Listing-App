a622f6e272d0f695e6105ff8d9517d0e14271b33
CryptonV8 <vslavenv@gmail.com>
2026-05-05 22:33:42 +0300

    Fix12

--- src/pages/CreateListingPage.tsx
+++ src/pages/CreateListingPage.tsx
@@ -1,4 +1,4 @@
-import { useState, type FormEvent } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
